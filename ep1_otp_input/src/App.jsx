50263763a779db1fd0fbb786b018bba971f91592
s4m1n <syan107405@gmail.com>
2025-06-01 13:15:33 +0600

    setting array value through input on change

--- ep1_otp_input/src/App.jsx
+++ ep1_otp_input/src/App.jsx
@@ -4,13 +4,29 @@ const OTP_LEN = 6;
 function App() {
   const [otpArr, setOtpArr] = useState(new Array(OTP_LEN).fill(""));
 
+  const handleOnChange = (value, index) => {
+    console.log(value, index);
+    if (!/^\d*$/.test(value)) return; // does not accept only non numeric values
+
+    const newArr = [...otpArr];
+    newArr[index] = value.trim().slice(-1);
+
+    setOtpArr(newArr);
+  };
+
   return (
     <div className="App">
       <h1>OTP Input</h1>
 
       {otpArr.map((digit, index) => {
         return (
-          <input className="otp-input" key={index} type="text" value={digit} />
+          <input
+            className="otp-input"
+            key={index}
+            type="text"
+            value={digit}
+            onChange={(e) => handleOnChange(e.target.value, index)}
+          />
         );
       })}
     </div>
