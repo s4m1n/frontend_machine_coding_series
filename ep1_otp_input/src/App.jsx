d792115a344f1f60868a6bd030e9d1d5eaac4bc5
s4m1n <syan107405@gmail.com>
2025-06-01 13:20:30 +0600

    focus on first input field on initial render and move to the next one upon inputting number

--- ep1_otp_input/src/App.jsx
+++ ep1_otp_input/src/App.jsx
@@ -1,17 +1,24 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const OTP_LEN = 6;
 function App() {
   const [otpArr, setOtpArr] = useState(new Array(OTP_LEN).fill(""));
+  const inputsRef = useRef([]);
+
+  useEffect(() => {
+    inputsRef.current[0]?.focus();
+  }, []);
 
   const handleOnChange = (value, index) => {
     console.log(value, index);
-    if (!/^\d*$/.test(value)) return; // does not accept only non numeric values
+    if (!/^\d*$/.test(value)) return; // does not accept non numeric values
 
     const newArr = [...otpArr];
     newArr[index] = value.trim().slice(-1);
 
     setOtpArr(newArr);
+
+    value && inputsRef.current[index + 1]?.focus();
   };
 
   return (
@@ -26,6 +33,7 @@ function App() {
             type="text"
             value={digit}
             onChange={(e) => handleOnChange(e.target.value, index)}
+            ref={(element) => (inputsRef.current[index] = element)}
           />
         );
       })}
